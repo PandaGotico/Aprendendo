b53b42054753d25e36b91cfe419afe4e3d40ddb5
PandaGotico <pvictor2112@hotmail.com>
2021-11-10 12:03:27 -0300

    Verificador de notas validas

--- src/Aprendendo/media.java
+++ src/Aprendendo/media.java
@@ -11,12 +11,33 @@ public class media {
 
         System.out.println("sua primeira nota: ");
         n1 = scan.nextDouble();
+            while ((n1 < 0) || (n1 > 10))
+            {
+            System.out.println("esta nota é invalida, digite uma nota valida: ");
+            n1 = scan.nextDouble();
+            }
+
         System.out.println("sua segunda nota: ");
         n2 = scan.nextDouble();
+            while ((n2 < 0) || (n2 > 10))
+            {
+            System.out.println("esta nota é invalida, digite uma nota valida: ");
+            n2 = scan.nextDouble();
+            }
         System.out.println("sua terceira nota: ");
         n3 = scan.nextDouble();
+            while ((n3 < 0) || (n3 > 10))
+            {
+            System.out.println("esta nota é invalida, digite uma nota valida: ");
+            n3 = scan.nextDouble();
+            }
         System.out.println("sua quarta nota: ");
         n4 = scan.nextDouble();
+            while ((n4 < 0) || (n4 > 10))
+            {
+            System.out.println("esta nota é invalida, digite uma nota valida: ");
+            n4 = scan.nextDouble();
+            }
         media = ((n1+n2+n3+n4)/4);
 
             if (media <= 5)
